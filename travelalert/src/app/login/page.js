c3fe529beb0f0964fe2b716d2afac78eeb79e8ee
Neeraj <chuahnneeraj192009@gmail.com>
2026-08-31 11:07:52 +0530

    login link

--- travelalert/src/app/login/page.js
+++ travelalert/src/app/login/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { Suspense, useState } from "react";
+import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
 import { supabase } from "@/lib/supabase";
 
